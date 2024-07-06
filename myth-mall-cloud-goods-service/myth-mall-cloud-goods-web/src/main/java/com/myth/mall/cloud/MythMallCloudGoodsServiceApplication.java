bc5aa5d5d52c817d9013a08e177d267dc9b2417f
Levi <979646206@qq.com>
2024-07-06 16:03:39 +0800

    整合商品业务代码

--- myth-mall-cloud-goods-service/myth-mall-cloud-goods-web/src/main/java/com/myth/mall/cloud/MythMallCloudGoodsServiceApplication.java
+++ myth-mall-cloud-goods-service/myth-mall-cloud-goods-web/src/main/java/com/myth/mall/cloud/MythMallCloudGoodsServiceApplication.java
@@ -1,5 +1,6 @@
 package com.myth.mall.cloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackageClasses = {com.myth.mall.cloud.openFeign.MythCloudAdminUserServiceFeign.class})
+@MapperScan("com.myth.mall.cloud.dao")
 public class MythMallCloudGoodsServiceApplication {
 
     public static void main(String[] args) {
